06a3e57926bbc70e2bbb4f95ad4952a38a885e0a
Nadden <nalaventure123@gmail.com>
2025-12-03 20:41:32 -0500

    changed id in schema to not be generated automatically

--- data.sql
+++ data.sql
@@ -8,8 +8,16 @@ INSERT INTO players VALUES ('MWheeler', 'strangerthings', 7582);
 INSERT INTO players VALUES ('Ghost', 'power', 1000000);
 INSERT INTO players VALUES ('JDart' , 'thisismypassword', 250);
 
-
 -- ASTRONAUTS
 INSERT INTO astronauts VALUES ('Mike Ibe', 'He loves what he does', 'Researcher', 5)
 INSERT INTO astronauts VALUES ('Nadden Auguste', 'Best software engineer ever!', 'Engineer', 5)
 INSERT INTO astronauts VALUES ('Terell Jones', 'He can fly a rocket. Not well though.', 'Rocket', 2)
+
+-- OWNED_ROCKETS
+
+
+-- EMPLOYED_ASTRONAUTS
+
+
+-- TRIPS
+
