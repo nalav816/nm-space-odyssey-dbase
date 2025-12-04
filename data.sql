b6b33fdce54e0f334d22efeffd227f122868e93e
Nadden <nalaventure123@gmail.com>
2025-12-03 20:08:43 -0500

    Added player reference fks for owned_rockets and employed_astronauts

---
+++ data.sql
@@ -0,0 +1,15 @@
+-- PLAYERS
+INSERT INTO players VALUES ('NAuguste', 'cupcake123', 52000);
+INSERT INTO players VALUES ('MIbe', 'chocolate572', 27000);
+INSERT INTO players VALUES ('DDennis', 'imhim', 23102);
+INSERT INTO players VALUES ('JSmith', 'wawasux', 800);
+INSERT INTO players VALUES ('THill', 'owimhurt', 16000);
+INSERT INTO players VALUES ('MWheeler', 'strangerthings', 7582);
+INSERT INTO players VALUES ('Ghost', 'power', 1000000);
+INSERT INTO players VALUES ('JDart' , 'thisismypassword', 250);
+
+
+-- ASTRONAUTS
+INSERT INTO astronauts VALUES ('Mike Ibe', 'He loves what he does', 'Researcher', 5)
+INSERT INTO astronauts VALUES ('Nadden Auguste', 'Best software engineer ever!', 'Engineer', 5)
+INSERT INTO astronauts VALUES ('Terell Jones', 'He can fly a rocket. Not well though.', 'Rocket', 2)
---
+++ queries.sql
@@ -0,0 +1,19 @@
+-- Get all of a player’s owned astronauts
+
+-- Get all of a player’s owned rockets
+
+-- Get all of a player’s astronauts not boarded on rockets
+
+-- Get a players dollar amount
+
+-- Get top 5 players with most amount of money
+
+-- Get top 5 players with the most amount of astronauts
+
+-- Get top 5 players with the most amount of rockets
+
+-- Get a rocket’s active trips (must be incompleted so curr time must be less than completion_time
+
+-- Get all available rockets (to load shop) 
+
+-- Get all available astronauts (to load shop)
